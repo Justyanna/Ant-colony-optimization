f1bde6a51b3d2e7d006763f4df18cc58375e22f7
Justyanna <annajkuczynska@onet.pl>
2020-12-08 10:10:41 +0100

    Add comment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-
+/*
+* Krok 1. Wygenerować listę itemów
+* Krok 2. Przejść po liście i sprawdzić czy item ma weight > niż totalCapacity plecaka -> jeśli tak to usunąć item
+* Krok 3. Dodać item do plecaka, oznaczyć flagę na true, odjąć wagę itemu od currentCapacity plecaka
+* */
     }
 }
