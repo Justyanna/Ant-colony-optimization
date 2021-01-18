8f028fbaabe2c7d114633e2b6f72788fdf362d27
Abraham Kaczmarski <abrahamkaczmarski@gmail.com>
2021-01-18 18:13:28 +0100

    Add ACO boilerplate

---
+++ src/algorithm/ACO.java
@@ -0,0 +1,78 @@
+package algorithm;
+
+import graph.Graph;
+import graph.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ACO {
+
+    private Graph data;
+    private Ant [] workers;
+    private int numCycles;
+
+    private List<Node> solution;
+    private int score;
+
+    public ACO(Graph data, int numAnts, int numCycles) {
+
+        this.data = data;
+        this.workers = new Ant[numAnts];
+        this.numCycles = numCycles;
+
+        unleashAnts();
+
+        compute();
+
+    }
+
+    public int getNumWorkers() { return workers.length; }
+    public int getNumCycles() { return numCycles; }
+    public List<Node> getSolution() { return new ArrayList<>(solution); }
+    public int getScore() { return score; }
+
+    private final void compute() {
+
+        solution = new ArrayList<>();
+        score = 0;
+
+        for(int c = 0; c < numCycles; c++) {
+
+            List<Node> cycleSolution = new ArrayList<>();
+            int cycleRecord = 0;
+
+            for(Ant w : workers) {
+//                TODO: Ant business
+                int antScore = 0;
+                if(antScore > cycleRecord) {
+                    cycleSolution = w.getPath();
+                    cycleRecord = antScore;
+                }
+            }
+
+            if(cycleRecord > score) {
+                solution = cycleSolution;
+                score = cycleRecord;
+            }
+
+            evaporate();
+            updatePheromones();
+
+        }
+
+    }
+
+    private void unleashAnts() {
+//        TODO: Ants' initialization
+    }
+
+    private void evaporate() {
+//        TODO: Evaporation
+    }
+
+    private void updatePheromones() {
+//        TODO: Pheromones
+    }
+
+}
